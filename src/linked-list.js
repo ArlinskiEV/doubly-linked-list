4e26d20a9ddef064fce57c4027a78b8cc2af957a
ArlinskiEV <ArlinskiEV@gmail.com>
2017-09-29 10:02:15 +0300

    just need reverse and correct

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,38 +1,45 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {
+    constructor() {//+
       this.prev = null;
       this.data = null;
       this.next = null;
     }
 
     // геттер
-    get length() {
+    get length() {//+
       var temp = this;
       var index = 0;
-      while (temp != null) {
+      while (temp != null) {  //to tail
         temp = temp.next;
+        index++;
+      }
+
+      if (temp != null) temp = this.prev;
+      while (temp != null) {  //to head
+        temp = temp.prev;
         index++;
       }
       return index;
     }
 
-    append(data) {
+    append(data) { //to tail+
       if (this.data == null) {
         this.data = data;
       } else {
         var temp = this;
         while (temp.next != null) {
-          temp=temp.next;
+          temp = temp.next;
         }
-			temp.next=new LinkedList();
-			temp.next.data=element;
+			temp.next = new LinkedList();
+			temp.next.data = data;
+      temp.next.prev = temp;
       }
       return this;
     }
 
-    addFirst(data) {
+    addFirst(data) {//+
   		if (this.data == null) {
   			this.data = data;	//пустой
   		} else {
@@ -41,7 +48,9 @@ class LinkedList {
   			temp.next = this.next;
         temp.data = this.data;
         temp.prev = this;
-        this.next.prev = temp;
+
+        //edit second
+        if (this.next != null) this.next.prev = temp;
 
         //edit head
         this.next = temp;
@@ -50,11 +59,11 @@ class LinkedList {
   		return this;
   	}
 
-    head() {
+    head() {//+
       return this.data;
     }
 
-    tail() {
+    tail() {//+
       var temp = this;
       while (temp.next != null) {
         temp=temp.next;
@@ -62,7 +71,7 @@ class LinkedList {
       return temp.data;
     }
 
-    at(index) {
+    at(index) {//+
       var temp = this;
       for (var i = 0; ((i < index) && (temp != null)); i++) {
         temp = temp.next;
@@ -70,41 +79,49 @@ class LinkedList {
       return temp.data;
     }
 
-    insertAt(index, data) {
+    insertAt(index, data) {//+
       var i = 0;
   		var temp = this;
+
+      if (index == 0) return this.addFirst(data);
+
   		while ((temp != null) && (i < index - 1)) {
   			temp = temp.next;
   			i++;
   		}
+
+      //temp with index-1
   		var l = new LinkedList();
   		l.data = data;
   		l.next = temp.next;
       l.prev = temp;
       temp.next.prev = l;
   		temp.next = l;
-  		return this;
+
+      return this;
     }
 
-    isEmpty() {
+    isEmpty() {//+
       return this.data == null;
     }
 
-    clear() {
+    clear() {//+
       this.prev = null;
       this.data = null;
       this.next = null;
       return this;
     }
 
-    deleteAt(index) {
+    deleteAt(index) {//+
       var temp = this;
-      while (i < index - 1) {
+      while ((temp.next != null) && (i < index - 1)) {
   			temp=temp.next;
   			i++;
   		}
+      //edit before
       temp.next = temp.next.next;
-      temp.next.next.prev = temp;
+      //edit after
+      if (temp.next.next != null) temp.next.next.prev = temp;
       return this;
     }
 
@@ -117,7 +134,7 @@ class LinkedList {
       return this;
     }
 
-    indexOf(data) {
+    indexOf(data) {//+
       var temp = this;
       var index = 0;
       while ((temp != null) && (temp.data != data)) {
